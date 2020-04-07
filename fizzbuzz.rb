ae798422419a8cd5eb9d4d6e24eca60dacf497fc
elazar cohen <escohen115@gmail.com>
2020-04-07 02:40:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,4 +3,5 @@
 #The goal of FizzBuzz is to build a program that can take a number and if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
 def fizzbuzz (num)
   if num % 3 == 0
-    
+    return "Fizz"
+    elsif
