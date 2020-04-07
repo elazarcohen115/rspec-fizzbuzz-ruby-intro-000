3d04d0f6b29adeb071f4f51b7a1458a8a90ce42e
elazar cohen <escohen115@gmail.com>
2020-04-07 02:53:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 #The goal of FizzBuzz is to build a program that can take a number and if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
-ruby
 def fizzbuzz (num)
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
